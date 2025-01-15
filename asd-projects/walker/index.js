4dd9bae4f48fcc5e2e442ee5231b93ce44f4581a
williamfloberg <142331310+williamfloberg@users.noreply.github.com>
2025-01-15 15:34:56 +0000

    tag ur it

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -15,22 +15,25 @@ function runProgram(){
   const WALKER_WIDTH = $("#walker").width();
   const WALKER_HEIGHT = $("#walker").height();
   const KEY = {
-  LEFT:37,
-  RIGHT:39,
-  UP:38,
-  DOWN:40,
-  
-  A:65,
-  D:68,
-  W:87,
-  S:83,
+    LEFT:37,
+    RIGHT:39,
+    UP:38,
+    DOWN:40,
+
+    A:65,
+    D:68,
+    W:87,
+    S:83,
   }
+  
   // Game Item Objects
   var walker = {
-  xPos: 0,
-  yPos: 0,
-  speedX: 0,
-  speedY: 0,
+    xPos: 0,
+    yPos: 0,
+    speedX: 0,
+    speedY: 0,
+    width: WALKER_WIDTH,
+    height: WALKER_HEIGHT,
   }
 
   var walker2 = {
@@ -38,12 +41,17 @@ function runProgram(){
     yPos: BOARD_HEIGHT - WALKER_HEIGHT,
     speedX: 0,
     speedY: 0,
-    }
+    width: WALKER_WIDTH,
+    height: WALKER_HEIGHT,
+  }
+  
+  var collisionDetected = false; // flag for collision detection
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
   $(document).on('keyup', handleKeyUp);
+  
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -53,115 +61,129 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-  redrawGameItem();
-  repositionGameItem();
-  wallCollision();  
-
-  }
+    redrawGameItem();
+    repositionGameItem();
+    wallCollision();
   
+    // collision checker
+    if (doCollide(walker, walker2) && !collisionDetected) {
+      showResult(true);
+      tagYoureIt();
+      collisionDetected = true; // set flag to true to prevent repeated alerts
+    } else if (!doCollide(walker, walker2)) {
+      showResult(false);
+      collisionDetected = false; // reset flag when no collision
+    }
+  }
+
   /* 
   Called in response to events.
   */
   function handleKeyDown(event) {
-  if (event.which === KEY.LEFT){
-    walker.speedX = -5;
-  }
-  if (event.which === KEY.RIGHT){
-    walker.speedX = 5;
-  }
-  if (event.which === KEY.UP){
-    walker.speedY = -5;
-  }
-  if (event.which === KEY.DOWN){
-    walker.speedY = 5;
-  }
-  //walker 2
-  if (event.which === KEY.A){
-    walker2.speedX = -5;
-  }
-  if (event.which === KEY.D){
-    walker2.speedX = 5;
-  }
-  if (event.which === KEY.W){
-    walker2.speedY = -5;
-  }
-  if (event.which === KEY.S){
-    walker2.speedY = 5;
-  }
-  }
-  function handleKeyUp(event) {
-  if (event.which === KEY.LEFT || event.which === KEY.RIGHT){
-    walker.speedX = 0;
-  }
-  if (event.which === KEY.UP || event.which === KEY.DOWN){
-    walker.speedY = 0;
+    if (event.which === KEY.LEFT){
+      walker.speedX = -5;
+    }
+    if (event.which === KEY.RIGHT){
+      walker.speedX = 5;
+    }
+    if (event.which === KEY.UP){
+      walker.speedY = -5;
+    }
+    if (event.which === KEY.DOWN){
+      walker.speedY = 5;
+    }
+    //walker 2
+    if (event.which === KEY.A){
+      walker2.speedX = -5;
+    }
+    if (event.which === KEY.D){
+      walker2.speedX = 5;
+    }
+    if (event.which === KEY.W){
+      walker2.speedY = -5;
+    }
+    if (event.which === KEY.S){
+      walker2.speedY = 5;
+    }
   }
 
-  if (event.which === KEY.A || event.which === KEY.D){
-    walker2.speedX = 0;
+  function handleKeyUp(event) {
+    if (event.which === KEY.LEFT || event.which === KEY.RIGHT){
+      walker.speedX = 0;
+    }
+    if (event.which === KEY.UP || event.which === KEY.DOWN){
+      walker.speedY = 0;
+    }
+
+    if (event.which === KEY.A || event.which === KEY.D){
+      walker2.speedX = 0;
+    }
+    if (event.which === KEY.W || event.which === KEY.S){
+      walker2.speedY = 0;
+    }
   }
-  if (event.which === KEY.W || event.which === KEY.S){
-    walker2.speedY = 0;
-  }
-  }
+
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
+
   function redrawGameItem(){
-  $("#walker").css("left", walker.xPos);
-  $("#walker").css("top", walker.yPos);
+    $("#walker").css("left", walker.xPos);
+    $("#walker").css("top", walker.yPos);
 
-  $("#walker2").css("left", walker2.xPos);
-  $("#walker2").css("top", walker2.yPos);
+    $("#walker2").css("left", walker2.xPos);
+    $("#walker2").css("top", walker2.yPos);
   }
+
   function repositionGameItem(){
-  walker.xPos += walker.speedX
-  walker.yPos += walker.speedY
+    walker.xPos += walker.speedX;
+    walker.yPos += walker.speedY;
 
-  walker2.xPos += walker2.speedX
-  walker2.yPos += walker2.speedY
+    walker2.xPos += walker2.speedX;
+    walker2.yPos += walker2.speedY;
   }
+
   function wallCollision(){
-  if (walker.xPos > BOARD_WIDTH - WALKER_WIDTH || walker.xPos < 0){
-    walker.xPos -= walker.speedX
-  }
-  if (walker.yPos > BOARD_HEIGHT - WALKER_HEIGHT || walker.yPos < 0){
-    walker.yPos -= walker.speedY
-  }
-  //walker 2
-  if (walker2.xPos > BOARD_WIDTH - WALKER_WIDTH || walker2.xPos < 0){
-    walker2.xPos -= walker2.speedX
-  }
-  if (walker2.yPos > BOARD_HEIGHT - WALKER_HEIGHT || walker2.yPos < 0){
-    walker2.yPos -= walker2.speedY
-  }
+    if (walker.xPos > BOARD_WIDTH - WALKER_WIDTH || walker.xPos < 0){
+      walker.xPos -= walker.speedX;
+    }
+    if (walker.yPos > BOARD_HEIGHT - WALKER_HEIGHT || walker.yPos < 0){
+      walker.yPos -= walker.speedY;
+    }
+    //walker 2
+    if (walker2.xPos > BOARD_WIDTH - WALKER_WIDTH || walker2.xPos < 0){
+      walker2.xPos -= walker2.speedX;
+    }
+    if (walker2.yPos > BOARD_HEIGHT - WALKER_HEIGHT || walker2.yPos < 0){
+      walker2.yPos -= walker2.speedY;
+    }
   }
 
   function handleMouseMove(event) {
     // get the mouse location from the event object
     moveBlueSquare(event.pageX, event.pageY);  
-    
-    if (doCollide(red, blue)) {
-        showResult(true);
-    } else {
-        showResult(false);
-    }
-}
-function doCollide(walker1, walker2){
-  if(
-    walker2.rightX > walker1.leftX &&
-    walker2.leftX < walker1.rightX &&
-    walker2.bottomY > walker1.topY &&
-    walker2.topY < walker1.bottomY
-    ){
-    return true;
-  } else {
-    return false;
   }
-}
+
+  function doCollide(walker1, walker2) {
+    if (
+      walker2.xPos < walker1.xPos + walker1.width &&
+      walker2.xPos + walker2.width > walker1.xPos &&
+      walker2.yPos < walker1.yPos + walker1.height &&
+      walker2.yPos + walker2.height > walker1.yPos
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   function showResult(result) {
-    $("p1").text('doCollide: ' + result);
-}
+    $("#result").text('doCollide: ' + result);
+  }
+
+  function tagYoureIt() {
+    alert("Tag, you're it!");
+  }
   
   function endGame() {
     // stop the interval timer
@@ -170,5 +192,4 @@ function doCollide(walker1, walker2){
     // turn off event handlers
     $(document).off();
   }
-  
 }
